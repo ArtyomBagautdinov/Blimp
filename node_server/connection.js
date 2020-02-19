0321b4d67ca2e44f53542acac8f7415d6e4a3b62
ArtyomBagautdinov <43336471+ArtyomBagautdinov@users.noreply.github.com>
2020-02-19 16:18:17 +0200

    сэйв

--- node_server/connection.js
+++ node_server/connection.js
@@ -3,7 +3,7 @@ const pool = new Pool({
     host: '127.0.0.1',
     port: 5432,
     database: 'BlimpDB',
-    user: 'postgres',
+    user: 'postgres', //artyom
     password: 'les_capitan99'
 });
 
